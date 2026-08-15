bea46de149f081f0e0887c26162ea08cba5b759d
dhruvmankame <dhruvmankameclg@gmail.com>
2026-08-15 15:24:44 +0530

    Implement user retrieval and command execution functions
    Added functions to retrieve user data and run commands.

---
+++ vuln.py
@@ -0,0 +1,14 @@
+import os
+import sqlite3
+
+API_KEY = "sk-1234567890abcdef1234567890abcdef"
+
+def get_user(username):
+    conn = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
+    cursor.execute(query)
+    return cursor.fetchone()
+
+def run_command(user_input):
+    os.system("ping " + user_input)
